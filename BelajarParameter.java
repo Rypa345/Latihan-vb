23389c6002ed1c67da91991ac8efcb3b7ae81d9a
Rypa345 <linuxuser345@gmail.com>
2016-03-29 22:40:16 +0700

    Added files via upload

---
+++ BelajarParameter.java
@@ -0,0 +1,26 @@
+
+package belajarparameter;
+
+/**
+ *
+ * @author rypa
+ */
+public class BelajarParameter {
+
+ 
+    public static void main(String[] args) {
+         BelajarParameter belajar = new BelajarParameter();
+     belajar.tambah("saya"," " ,"kamu");
+     
+      
+      
+    
+    }
+    private void tambah (String a, String b, String c){
+        String hasil;
+        hasil= a + b + c;
+        System.out.println("kita :" + hasil);
+    }
+    }
+    
+
---
+++ BelajarString.java
@@ -0,0 +1,9 @@
+package fungsiprosedur;
+
+/**
+ *
+ * @author rypa
+ */
+public class BelajarString {
+    
+}
