be8257b7540f703ad820a07698cb98d0ba60ca95
Rypa345 <linuxuser345@gmail.com>
2016-03-29 11:00:21 +0700

    Latihan Java

---
+++ LatihanMethod.java
@@ -0,0 +1,23 @@
+public class LatihanMethod{
+
+public static void main (String [] args){
+
+LatihanMethod nama = new LatihanMethod();
+
+System.out.println(nama.namaSaya());
+nama.pertambahan();
+}
+//fungsi -> sesuatu yang mengembalikan nilai
+
+public String namaSaya(){
+return "rypa";
+
+}
+
+public void pertambahan(){
+int a = 1;
+int b = 1;
+System.out.println(a+b);
+
+}
+  }
---
+++ Latihandua.java
@@ -0,0 +1,16 @@
+public class Latihandua {
+
+    public static void main(String[] args) {
+     
+      String ab = "++++++++";
+      String bc = "+  RY  +";
+      String cd = "+  PA  +";
+      String de = "++++++++";
+     
+         System.out.println(ab);
+        System.out.println(bc);
+ System.out.println(cd);
+       System.out.println(de);
+}
+}
+        
